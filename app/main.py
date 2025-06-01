0db31b11b667b76ca802a2bc22dbe49937610126
鲨鱼辣椒 <monishine_code@163.com>
2025-06-01 20:58:05 +0800

    feat: render

--- app/main.py
+++ app/main.py
@@ -3,6 +3,13 @@ from fastapi.routing import APIRoute
 from app.api.main import api_router
 from app.core.config import settings
 from app.core.database import create_db_and_tables
+import os
+
+
+title = os.environ.get("SERVICE_PROJECT_NAME", "FastAPI") or settings.SERVICE_PROJECT_NAME
+prefix = os.environ.get("SERVICE_API_PREFIX", "/api") or settings.SERVICE_API_PREFIX
+host = os.environ.get("SERVICE_HOST", "0.0.0.0") or settings.SERVICE_HOST
+port = int(os.environ.get("SERVICE_PORT", 8000)) or settings.SERVICE_PORT
 
 
 def generate_unique_id(route: APIRoute):
@@ -10,8 +17,8 @@ def generate_unique_id(route: APIRoute):
 
 
 app = FastAPI(
-    title=settings.SERVICE_PROJECT_NAME,
-    openapi_url=f"{settings.SERVICE_API_PREFIX}/openapi.json",
+    title=title,
+    openapi_url=f"{prefix}/openapi.json",
     generate_unique_id_function=generate_unique_id,
 )
 
@@ -21,8 +28,7 @@ def on_startup():
     create_db_and_tables()
 
 
-app.include_router(api_router, prefix=settings.SERVICE_API_PREFIX)
-
+app.include_router(api_router, prefix=prefix)
 
 if __name__ == "__main__":
-    uvicorn.run("app:main.app", host=settings.SERVICE_HOST, port=settings.SERVICE_PORT, reload=True)
+    uvicorn.run("app:main.app", host=host, port=port, reload=True)
